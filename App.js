844620d7e71f26a8f02d06f7e16dba6600bf48dc
everywherejacobkim <everywherejacobkim@gmail.com>
2022-05-31 15:31:07 -0700

    feat: making TextInput functional

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import react from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { theme } from './Colors';
 
 export default function App() {
 
   const [working, setWorking] = useState(true);
+  const [inputText, setInputText] = useState("");
 
   const travel = () => {
     setWorking(false)
@@ -17,12 +18,25 @@ export default function App() {
     console.log(working);
   }
 
+  const onChangeText = (text) => {
+    setInputText(text);
+    console.log(inputText)
+  }
+
+  const addTodo = () => {
+    if(inputText === "") {
+      return;
+    } else {
+      setInputText("");
+    }
+  }
+
 
   return (
     <View style={styles.container}>
-     <StatusBar style="auto" />
-      <View style={styles.header}>
+    <StatusBar style="auto" />
 
+      <View style={styles.header}>
         <TouchableOpacity onPress={work}>
           <Text style={{...styles.btnTxt, color: working ? theme.white : theme.grey}}>Work</Text>
         </TouchableOpacity>
@@ -30,6 +44,20 @@ export default function App() {
         <TouchableOpacity onPress={travel}>
         <Text style={{...styles.btnTxt, color: working ? theme.grey : theme.white}}>Travel</Text>
         </TouchableOpacity>
+      </View>
+
+      <View>
+        <TextInput 
+          style={styles.input} 
+          placeholder={working ? "Add a Todo.." : "Where do you want to go?"}
+          onChangeText={onChangeText}
+          onSubmitEditing={addTodo}  
+          returnKeyType="done" 
+        >
+          {inputText}
+        </TextInput>
+
+
 
       </View>
     
@@ -51,5 +79,13 @@ const styles = StyleSheet.create({
   btnTxt: {
     fontSize: 45,
     fontWeight: "600",
+  },
+  input: {
+    backgroundColor: theme.white,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 15,
+    fontSize: 16,
   }
 });
