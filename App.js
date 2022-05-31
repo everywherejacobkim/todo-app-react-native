3dcfc3e6382ba3fb6348c47bd0ffacd1e0f21c56
everywherejacobkim <everywherejacobkim@gmail.com>
2022-05-31 14:43:54 -0700

    feat: structure styling & create state

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { theme } from './Colors';
 
 export default function App() {
+
+  const [working, setWorking] = useState(true);
+
+  const travel = () => {
+    setWorking(false)
+    console.log(working);
+  }
+  const work = () => {
+    setWorking(true)
+    console.log(working);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <StatusBar style="auto" />
+      <View style={styles.header}>
+
+        <TouchableOpacity onPress={work}>
+          <Text style={styles.btnTxt}>Work</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={travel}>
+        <Text style={styles.btnTxt}>Travel</Text>
+        </TouchableOpacity>
+
+      </View>
+    
+    
+    
+    
     </View>
   );
 }
@@ -13,8 +43,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
+  },
+  btnTxt: {
+    color: "white",
+    fontSize: 45,
+    fontWeight: "600",
+  }
 });
---
+++ Colors.js
@@ -0,0 +1,4 @@
+export const theme = {
+    bg: "#1c1c1c",
+    grey: "#3A3D40",
+}
